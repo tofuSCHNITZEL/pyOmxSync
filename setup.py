a709a5bcff441443bbddac5539827d816db976dc
Tobias Perschon <tobias@perschon.at>
2022-02-15 16:51:41 +0100

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='omxsync',
-      version='0.1.1',
+      version='0.2.0',
       description='Master/slave syncing of omxplayer instances over network',
       url='https://github.com/tofuSCHNITZEL/pyOmxSync',
       author='Mark van de Korput',
